04c7b936dadb97e247059237825c20adc583127d
MatEw <maateew@gmail.com>
2017-04-10 20:49:18 +0200

    Fix

--- src/app/playlists/formfeedback.component.ts
+++ src/app/playlists/formfeedback.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'formfeedback',
   template: `
     <div class="alert" 
-      *ngIf="complete>=0"
+      *ngIf="visible"
       [ngClass]="{'alert-danger':!complete,'alert-success':complete}"
       role="alert">
       <strong>{{title}}</strong> {{text}}
@@ -14,8 +14,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FormfeedbackComponent implements OnInit {
 
-  complete:any = -1;
-  form=null;
+  complete:boolean;
+  visible:boolean=false;
+  form;
+
 
   title = "Congratulation";
   text = "You complete the form correctly";
@@ -24,9 +26,11 @@ export class FormfeedbackComponent implements OnInit {
 
   @Input("ref")
   set getform(e){
-    e.valueChanges.subscribe(data=> {
+    e.valueChanges.subscribe(()=> {
       if(!e.submitted)
-        this.validate(data)
+        this.validate()
+      else
+        this.visible=false;
     });
     this.form=e.form.controls;
   }
@@ -37,31 +41,29 @@ export class FormfeedbackComponent implements OnInit {
     required: "Zaznaczone pole jest wymagane. "
   }
 
-  validate(controls){
+  validate(){
     let text="";
     this.complete=true;
-
     for(let controln in this.form)
     { 
       let control = this.form[controln];
       if(!control.valid && !control.pristine)
       {
-        console.log(control);
         this.complete=false;
+        this.visible=true;
 
         for(let error in control.errors)
           text+=this.ErrorMessage[error];
       }
     }
-    console.log(text);
     if(text==="")
     {
-      this.title="Congratulation"
-      this.text="You complete the form correctly";
+      this.title="Gratulacje"
+      this.text="Formularz jest poprawnie wypełniony";
     }
     else 
     {
-      this.title="Error";
+      this.title="Błąd";
       this.text=text;
     }
 
